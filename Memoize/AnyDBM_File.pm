4a41a7f9f3d2f0b0ac3a39c2df4211e80d92a298
Aristotle Pagaltzis <pagaltzis@gmx.de>
2022-07-27 07:06:36 +0200

    simplify Memoize::AnyDBM_File

--- Memoize/AnyDBM_File.pm
+++ Memoize/AnyDBM_File.pm
@@ -2,16 +2,11 @@ package Memoize::AnyDBM_File;
 
 use vars qw(@ISA $VERSION);
 $VERSION = '1.05';
-@ISA = qw(DB_File GDBM_File Memoize::NDBM_File Memoize::SDBM_File ODBM_File) unless @ISA;
 
-my $mod;
-for $mod (@ISA) {
-#  (my $truemod = $mod) =~ s/^Memoize:://;
-#  my $file = "$mod.pm";
-#  $file =~ s{::}{/}g;
+for my $mod (qw(DB_File GDBM_File Memoize::NDBM_File Memoize::SDBM_File ODBM_File)) {
   if (eval "require $mod") {
     print STDERR "AnyDBM_File => Selected $mod.\n" if $Verbose;
-    @ISA = ($mod);	# if we leave @ISA alone, warnings abound
+    @ISA = $mod;
     return 1;
   }
 }
